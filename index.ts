ff1c2155807e55325148c5b31961361f9f51197a
wcle3456 <wcle3456@gmail.com>
2024-11-04 11:30:01 +0000

    Update index.ts : Apply functions

--- index.ts
+++ index.ts
@@ -1,75 +1,86 @@
 const offers = new Map<string, { sd: RTCSessionDescriptionInit, res: (value: Response | PromiseLike<Response>) => void, timer: Timer }>();
 
-const CORS_HEADERS = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET, POST',
-    'Access-Control-Allow-Headers': 'Content-Type',
-};
-const badRequest = new Response("Bad Request", { status: 400, headers: CORS_HEADERS});
-const notFound = new Response("Not Found", { status: 404, headers: CORS_HEADERS });
-const requestTimeout = new Response("Request Timeout", { status: 408, headers: CORS_HEADERS });
+function applyCORSHeader(res: Response) {
+    res.headers.set('Access-Control-Allow-Origin', '*');
+    res.headers.set('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+    res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
+    return res;
+}
+
+function badRequest() {
+    const response = new Response("Bad Request", { status: 400 });
+    return applyCORSHeader(response);
+}
+function notFound() {
+    const response = new Response("Not Found", { status: 404 });
+    return applyCORSHeader(response);
+}
+function requestTimeout() {
+    const response = new Response("Request Timeout", { status: 408 });
+    return applyCORSHeader(response);
+}
 
 Bun.serve({
     port: process.env.PORT,
     async fetch(req) {
         // Handle CORS preflight requests
         if (req.method === 'OPTIONS') {
-            const res = new Response('Departed', { headers: CORS_HEADERS });
-            return res;
+            const response = new Response('Departed');
+            return applyCORSHeader(response);
         }
-        
+
         const url = new URL(req.url);
 
         if (url.pathname === "/create-offer") {
-            // console.log(url.pathname);
-            if (req.method !== 'POST') return badRequest;
+            if (req.method !== 'POST') return badRequest();
 
             const body = await req.json();
-            if (!body.id || !body.sd) return badRequest;
+            if (!body.id || !body.sd) return badRequest();
 
             return new Promise<Response>((resolve) => {
                 const timeout = setTimeout(() => {
                     offers.delete(body.id);
-                    // console.log(offers);
-                    resolve(requestTimeout);
-                }, 60000);
+                    const timeoutRes = requestTimeout();
+                    resolve(timeoutRes);
+                }, 30000);
                 offers.set(body.id, { sd: body.sd, res: resolve, timer: timeout });
-                // console.log(offers);
             });
         }
 
         if (url.pathname === "/get-offer") {
-            // console.log(url.pathname);
-            if (req.method !== 'GET') return badRequest;
+            if (req.method !== 'GET') return badRequest();
 
             const id = url.searchParams.get('id');
-            if (!id) return badRequest;
+            if (!id) return badRequest();
 
             const waitingOffer = offers.get(id);
 
-            if (waitingOffer) return new Response(JSON.stringify(waitingOffer.sd), { headers: CORS_HEADERS });
-            else return notFound;
+            if (waitingOffer) {
+                const response = new Response(JSON.stringify(waitingOffer.sd));
+                return applyCORSHeader(response);
+            }
+            else return notFound();
         }
 
         if (url.pathname === "/create-answer") {
-            // console.log(url.pathname);
-            if (req.method !== 'POST') return badRequest;
+            if (req.method !== 'POST') return badRequest();
 
             const body = await req.json();
-            if (!body.id || !body.sd) return badRequest;
+            if (!body.id || !body.sd) return badRequest();
 
             const waitingOffer = offers.get(body.id);
 
             if (waitingOffer) {
                 clearTimeout(waitingOffer.timer);
-                waitingOffer.res(new Response(JSON.stringify(body.sd), { headers: CORS_HEADERS }));
+                const resOffer = new Response(JSON.stringify(body.sd));
+                waitingOffer.res(applyCORSHeader(resOffer));
                 offers.delete(body.id);
-                // console.log(offers);
-                return new Response("Wait for connecting", { headers: CORS_HEADERS });
+                const response = new Response("Wait for connecting");
+                return applyCORSHeader(response);
             }
-            else return notFound;
+            else return notFound();
         }
 
-        return badRequest;
+        return badRequest();
     }
 });
