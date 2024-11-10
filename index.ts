7a78c9a7cc433fc258875f935a3aaedb70f8fcfe
rdg1029 <wcle3456@gmail.com>
2024-11-10 14:44:58 +0900

    Update index.ts : Apply allowed origin list

--- index.ts
+++ index.ts
@@ -1,37 +1,41 @@
 const offers = new Map<string, { sd: RTCSessionDescriptionInit, res: (value: Response | PromiseLike<Response>) => void, timer: Timer }>();
 
-function applyCORSHeader(res: Response) {
-    res.headers.set('Access-Control-Allow-Origin', '*');
+const ALLOW_ORIGIN = Bun.env.ALLOWED_ORIGIN_URL?.split(',');
+
+function applyCORSHeader(res: Response, origin: string = ALLOW_ORIGIN![0]) {
+    res.headers.set('Access-Control-Allow-Origin', origin);
     res.headers.set('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
     res.headers.set('Access-Control-Allow-Headers', 'Content-Type');
     return res;
 }
-function response(content: string, code: number) {
+function response(req: Request, content: string, code: number) {
     const response = new Response(content, { status: code });
-    return applyCORSHeader(response);
+    const reqOrigin = req.headers.get('origin');
+    return applyCORSHeader(response, ALLOW_ORIGIN?.includes(reqOrigin!) ? reqOrigin! : undefined);
 }
 
 Bun.serve({
-    port: process.env.PORT,
+    // port: process.env.PORT,
+    port: 3000,
     async fetch(req) {
         // Handle CORS preflight requests
         if (req.method === 'OPTIONS') {
-            return response('Departed', 200);
+            return response(req, 'Departed', 200);
         }
 
         const url = new URL(req.url);
 
         if (url.pathname === "/create-offer") {
-            if (req.method !== 'POST') return response("Bad Request", 400);
+            if (req.method !== 'POST') return response(req, "Bad Request", 400);
 
             const body = await req.json();
-            if (!body.id || !body.sd) return response("Bad Request", 400);
-            if (offers.has(body.id)) return response("ID Already Exists", 202)
+            if (!body.id || !body.sd) return response(req, "Bad Request", 400);
+            if (offers.has(body.id)) return response(req, "ID Already Exists", 202)
 
             return new Promise<Response>((resolve) => {
                 const timeout = setTimeout(() => {
                     offers.delete(body.id);
-                    const timeoutRes = response(`Code (${body.id}) is inactive (timeout)`, 408);
+                    const timeoutRes = response(req, `Code (${body.id}) is inactive (timeout)`, 408);
                     resolve(timeoutRes);
                 }, 30000);
                 offers.set(body.id, { sd: body.sd, res: resolve, timer: timeout });
@@ -39,51 +43,51 @@ Bun.serve({
         }
 
         if (url.pathname === "/delete-offer") {
-            if (req.method !== 'GET') return response("Bad Request", 400);
+            if (req.method !== 'GET') return response(req, "Bad Request", 400);
 
             const id = url.searchParams.get('id');
-            if (!id) return response("Bad Request", 400);
-            if (!offers.has(id)) return response("Not Found", 404);
+            if (!id) return response(req, "Bad Request", 400);
+            if (!offers.has(id)) return response(req, "Not Found", 404);
 
             const waitingOffer = offers.get(id);
             clearTimeout(waitingOffer!.timer);
-            const resOffer = response("", 204); // No contents
+            const resOffer = response(req, "", 204); // No contents
             waitingOffer!.res(resOffer);
             offers.delete(id);
 
-            return response(`Code (${id}) is inactive (deleted)`, 200);
+            return response(req, `Code (${id}) is inactive (deleted)`, 200);
         }
 
         if (url.pathname === "/get-offer") {
-            if (req.method !== 'GET') return response("Bad Request", 400);
+            if (req.method !== 'GET') return response(req, "Bad Request", 400);
 
             const id = url.searchParams.get('id');
-            if (!id) return response("Bad Request", 400);
+            if (!id) return response(req, "Bad Request", 400);
 
             const waitingOffer = offers.get(id);
 
-            if (waitingOffer) return response(JSON.stringify(waitingOffer.sd), 200);
-            else return response("Not Found", 404);
+            if (waitingOffer) return response(req, JSON.stringify(waitingOffer.sd), 200);
+            else return response(req, "Not Found", 404);
         }
 
         if (url.pathname === "/create-answer") {
-            if (req.method !== 'POST') return response("Bad Request", 400);
+            if (req.method !== 'POST') return response(req, "Bad Request", 400);
 
             const body = await req.json();
-            if (!body.id || !body.sd) return response("Bad Request", 400);
+            if (!body.id || !body.sd) return response(req, "Bad Request", 400);
 
             const waitingOffer = offers.get(body.id);
 
             if (waitingOffer) {
                 clearTimeout(waitingOffer.timer);
-                const resOffer = response(JSON.stringify(body.sd), 200)
+                const resOffer = response(req, JSON.stringify(body.sd), 200)
                 waitingOffer.res(resOffer);
                 offers.delete(body.id);
-                return response("Wait for connecting", 200);
+                return response(req, "Wait for connecting", 200);
             }
-            else return response("Not Found", 404);
+            else return response(req, "Not Found", 404);
         }
 
-        return response("Bad Request", 400);
+        return response(req, "Bad Request", 400);
     }
 });
